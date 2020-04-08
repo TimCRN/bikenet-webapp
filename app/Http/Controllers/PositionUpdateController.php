acfb3ea8505656fa5a2f306884a05b32922080d4
Tim <tim.crounse@gmail.com>
2020-04-08 11:06:39 +0200

    Models created with their respected migrations, controllers & resources

---
+++ app/Http/Controllers/PositionUpdateController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PositionUpdate;
+use Illuminate\Http\Request;
+
+class PositionUpdateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PositionUpdate  $positionUpdate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PositionUpdate $positionUpdate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PositionUpdate  $positionUpdate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PositionUpdate $positionUpdate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PositionUpdate  $positionUpdate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PositionUpdate $positionUpdate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PositionUpdate  $positionUpdate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PositionUpdate $positionUpdate)
+    {
+        //
+    }
+}
