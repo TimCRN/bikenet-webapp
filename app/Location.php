6c7db3ce07549202e6dc59f8b4997c2c70e0615c
Tim <tim.crounse@gmail.com>
2020-04-09 15:03:40 +0200

    Location model finished

--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    public $latitude;
+    public $longitude;
+
+    public function __construct($lat, $lon)
+    {
+        $this->latitude = $lat;
+        $this->longitude = $lon;
+    }
 }
