e19e8269d98a2458d7bee2043497515494524fe4
Tim <tim.crounse@gmail.com>
2020-04-10 15:00:53 +0200

    Added fillable constructor arguments for 'Booking'

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'bike_id',
+        'start_position',
+        'end_position'
+    ];
 }
