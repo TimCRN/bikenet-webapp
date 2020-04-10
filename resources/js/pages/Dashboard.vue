868f3aed1201fae170d1a1158d47dbd4e37db4b4
Tim <tim.crounse@gmail.com>
2020-04-10 17:53:25 +0200

    Api fetching test in Dashboard.vue

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,66 +1,31 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
-                    <div class="card-header">Add Task</div>
-                    <div class="card-body">
-                        <input v-model="task" class="form-control" v-on:keyup.enter="submit">
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">All Tasks</div>
-                    <div class="card-body">
-                        <li style="list-style: none" v-for="(task, id) in tasks" :key="id">{{task.task}}</li>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </template>
+
 <script>
     export default {
-        data (){
+        data() {
             return {
-                task: null,
-                tasks: null
-            }
+                bikes: []
+            };
         },
-        methods:{
-            //we submit the task to the add-task api then clear the input field
-            submit(){
-                axios.post('api/add-task', {
-                    task: this.task,
-                })
-                    .then(response => {
-                        this.task = null
-                    })
-                    .catch(function (error) {
-                    });
-            },
-            getTask(){
-                axios.get('api/get-task').then(response=>{
-                    this.tasks = response.data.tasks
-                })
-            }
-        },
-        watch: {
-            // whenever task changes, this function will run
-            task: function (newTask, oldTask) {
-                this.getTask()
-            }
-        },
+
         created() {
-            this.getTask()
+            this.getBikes();
         },
-        //before the route is mounted we check if the user is logged in
-        beforeRouteEnter (to, from, next) {
-            if (!window.Laravel.isLoggedin) {
-                return next('/');
+
+        methods: {
+            getBikes() {
+                axios.get('/api/bikes')
+                    .then(response => {
+                        this.bikes = response.data
+                    })
+                    .catch(err => console.log(err));
             }
-            next();
         }
-    }
+    };
 </script>
+
