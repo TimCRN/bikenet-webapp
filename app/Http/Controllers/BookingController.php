5f0899bb8310bf44a7533d79206f9070ddfff02b
Tim <tim.crounse@gmail.com>
2020-04-13 17:10:30 +0200

    Basic functionality added to BookingController.php

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -7,24 +7,19 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('auth:sanctum');
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function create()
+    public function index()
     {
-         // nog meer test
+        return response()->json(Booking::all()->toJson());
     }
 
     /**
@@ -35,51 +30,73 @@ class BookingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $b = new Booking([
+            'user_id' => $request.user_id,
+            'bike_id' => $request.bike_id,
+            'start_position' => $request.position,
+        ]);
+
+        try {
+            $b->save();
+        }
+        catch (\Throwable $exception){
+            report($exception);
+            return response('Booking could not be created', 500);
+        }
+
+        return response('Booking created');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Booking  $reservation
-     * @return \Illuminate\Http\Response
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Booking $reservation)
+    public function show(Booking $booking)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Booking  $reservation
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Booking $reservation)
-    {
-        //
+        return response()->json($booking->toJson());
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Booking  $reservation
+     * @param  \App\Booking  $booking
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Booking $reservation)
+    public function update(Request $request, Booking $booking)
     {
-        //
+        try {
+            $booking->end_position = $request.position;
+            $booking->completed = true;
+        }
+        catch (\Throwable $exception){
+            report($exception);
+            return response('Booking could not be updated', 500);
+        }
+
+        return response('Booking has been updated');
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Booking  $reservation
+     * @param  \App\Booking  $booking
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Booking $reservation)
+    public function destroy(Booking $booking)
     {
-        //
+        try {
+            $booking->delete();
+        }
+        catch (\Throwable $exception)
+        {
+            report($exception);
+            return response('Booking could not be deleted', 500);
+        }
+
+        return response('Booking has been deleted');
     }
 }
