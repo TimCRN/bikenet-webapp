bc3d43458578e17c0e922ebdf21df224299e1492
Accendit <36697826+Accendit@users.noreply.github.com>
2020-04-13 18:08:51 +0200

    removed Bootrstrap from Vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>Laravel Airlock</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -26,6 +25,5 @@
 @endif
 <div id="app"></div>
 <script src="{{ mix('js/app.js') }}"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
