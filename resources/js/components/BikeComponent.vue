cd0915a4589694dc8fb2a59b11198c7cec10a991
Accendit <36697826+Accendit@users.noreply.github.com>
2020-04-13 15:53:05 +0200

    Added a Vue Bike Component

---
+++ resources/js/components/BikeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <p>{{ this.name }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'name',
+            'available',
+            'zoneId',
+            'created_at',
+            'updated_at'
+        ],
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
