7c813bae406ee1c97303300f21e5e3c0058d4024
Tim <tim.crounse@gmail.com>
2020-04-13 15:57:02 +0200

    PositionUpdate model cleaned

--- app/PositionUpdate.php
+++ app/PositionUpdate.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class PositionUpdate extends Model
 {
-    public $bike;
-    public $position;
 
-    public function __construct($bike, $position)
-    {
-        $this->bike = $bike;
-        $this->position = $position;
-    }
 }
