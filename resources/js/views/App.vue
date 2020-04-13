0cb647ae111d283dfa4f044e9f9e26d91a7274ca
Accendit <36697826+Accendit@users.noreply.github.com>
2020-04-13 15:52:48 +0200

    Added 'BikeNet' to navbar

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -3,7 +3,7 @@
     <div>
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <router-link :to="{name: 'home'}" class="navbar-brand">AirLock</router-link>
+                <router-link :to="{name: 'home'}" class="navbar-brand">BikeNet</router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
