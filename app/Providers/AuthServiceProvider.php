9ae2bebcb96b2f3734b1cf37230444048b4cd067
Tim <tim.crounse@gmail.com>
2020-04-15 16:01:27 +0200

    Policies are now enacted

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Bike::class => \App\Policies\BikePolicy::class,
+        \App\Booking::class => \App\Policies\BookingPolicy::class
     ];
 
     /**
