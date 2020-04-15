1a3b8ed85e604d2424b72f25f42e4375d63e69f6
Tim <tim.crounse@gmail.com>
2020-04-15 15:13:23 +0200

    Hidden unnecessary member variables in PositionUpdate.php

--- app/PositionUpdate.php
+++ app/PositionUpdate.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PositionUpdate extends Model
 {
-
+    protected $hidden = [
+        'id',
+        'bike_id',
+        'created_at',
+        'updated_at'
+    ];
 }
