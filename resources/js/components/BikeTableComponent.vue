a5233bf1095502134dded3a7b7f0f24b9e5bde3c
Accendit <36697826+Accendit@users.noreply.github.com>
2020-04-19 20:20:36 +0200

    Fixed position display

--- resources/js/components/BikeTableComponent.vue
+++ resources/js/components/BikeTableComponent.vue
@@ -1,23 +1,28 @@
 <template>
     <div class="container">
+        <BikeModal v-if="showEditModal" :name="changeBike.name" :available="changeBike.available" @close="closeModal()" @submit="submitModal()"></BikeModal>
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">Id</th>
                     <th scope="col">Name</th>
                     <th scope="col">Available</th>
+                    <th scope="col">Longitude</th>
+                    <th scope="col">Latitude</th>
                     <th scope="col">Actions</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(bike, index) in bikes" :key="bike.id">
+                <tr v-for="bike in bikes" :key="bike.id">
                     <th scope="row">{{ bike.id }}</th>
                     <td>{{ bike.name }}</td>
                     <td>{{ bike.available }}</td>
+                    <td>{{ bike.position? bike.position.longitude: '' }}</td>
+                    <td>{{ bike.position? bike.position.latitude: '' }}</td>
                     <td>
                         <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-danger" @click="removeBike(index)">Remove</button>
-                            <button type="button" class="btn btn-primary" @click="changeBike(bike.id)">Settings</button>
+                            <button type="button" class="btn btn-danger" @click="removeBike(bike.id)">Remove</button>
+                            <button type="button" class="btn btn-primary" @click="changeBike(bike)">Settings</button>
                         </div>
                     </td>
                 </tr>
@@ -27,24 +32,55 @@
 </template>
 
 <script>
+    import BikeModal from './BikeSingleModalComponent.vue'
+
     export default {
         props: [
             'bikes'
         ],
+
+        components: {
+            BikeModal
+        },
+
+        data: () => {
+            return {
+                editBike: null,
+                showEditModal: false
+            }
+        },
+
         methods: {
-            removeBike(index) {
-                let bikeid = this.bikes[index].id
+            removeBike(bikeid) {
                 axios.delete('/api/bikes/' + bikeid.toString())
                     .then(response => {
-                        console.log(response)
                         this.$emit('refresh')
+                        this.$toasted.show(response.data)
                     })
                     .catch(response => {
-
+                        this.$toasted.show(response.data)
                     })
             },
-            changeBike(bikeid) {
-                // axios.
+
+            changeBike(bike) {
+                this.editBike = bike
+                this.showEditModal = true
+            },
+
+            closeModal() {
+                this.showEditModal = false
+            },
+
+            submitModal() {
+                axios.put('/api/bikes/' + this.editBike.id.toString(), this.editBike)
+                    .then(response => {
+                        this.$emit('refresh')
+                        this.$toasted.show(response.data)
+                    })
+                    .catch(response => {
+                        this.$toasted.show(response.data)
+                    })
+
             }
         }
     }
