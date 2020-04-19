631101d467ab706e5ce2f6524fabf1d8cbff8b7a
Accendit <36697826+Accendit@users.noreply.github.com>
2020-04-19 20:20:08 +0200

    Added a Bikes page and route

---
+++ resources/js/pages/Bikes.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+
+        <BikeTable @refresh="getBikes()" :bikes=bikes></BikeTable>
+
+    </div>
+</template>
+
+<script>
+    import BikeTable from '../components/BikeTableComponent.vue'
+
+    export default {
+        data() {
+            return {
+                bikes: []
+            };
+        },
+
+        components: {
+            BikeTable
+        },
+
+        created() {
+            this.getBikes();
+        },
+
+        methods: {
+
+        }
+    };
+</script>
+
